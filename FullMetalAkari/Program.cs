4048f59b502a61da2df88a0d75ac497aa7f3d90f
Jackson Maclean <xjax1@icloud.com>
2023-01-12 01:37:10 -0800

    Add project files.

---
+++ FullMetalAkari/Program.cs
@@ -0,0 +1,32 @@
+﻿//Cody By: Jackson Maclean
+//Generic
+using System;
+using System.Windows;
+//OpenTK
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace FullMetalAkari
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(1920, 1080),
+                Title = "Full Metal Akari",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            // To create a new window, create a class that extends GameWindow, then call Run() on it.
+            using (var window = new windowHandler(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+
+        }
+    }
+}
