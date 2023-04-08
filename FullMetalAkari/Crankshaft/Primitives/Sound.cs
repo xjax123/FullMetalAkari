1fd0becf2d7c5881a6f3721520322fb0bd4a1bb2
Jackson Maclean <xjax1@icloud.com>
2023-04-08 04:06:30 -0700

    Working to convert from WMP to OpenAL (Currently Nonfunctional)

--- FullMetalAkari/Crankshaft/Primitives/Sound.cs
+++ FullMetalAkari/Crankshaft/Primitives/Sound.cs
@@ -1,93 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
+using System.Threading.Channels;
+using OpenTK.Audio;
+using OpenTK.Audio.OpenAL;
+using OpenTK.Graphics.OpenGL;
 using WMPLib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Crankshaft.Primitives
 {
     public class Sound
     {
-        protected string path;
-        protected string name;
-        private byte volume;
-        private WindowsMediaPlayer player;
+        //Protected variables
+        protected int source;
+        protected int buffer;
+        protected byte[] soundData;
+        protected int channels;
+        protected int bytes;
+        protected int samples;
+
+        //Public variables
+        private string path;
+        private string name;
+        private int volume;
+        private bool loopState;
+
         public string Path { get => path; set => path = value; }
         public string Name { get => name; set => name = value; }
-        public byte Volume { get => volume; set => volume = value; }
-        public WindowsMediaPlayer Player { get => player; set => player = value; }
+        public int Volume { get => volume; set => volume = value; }
+        public bool LoopState { get => loopState; set => loopState = value; }
+
 
         /// <summary>
-        /// Empty Constructor
+        /// 
         /// </summary>
-        public Sound()
+        /// <param name="path">Filepath for the sound file</param>
+        /// <param name="name">Name of the sound</param>
+        public Sound(string path, string name) 
         {
-        }
-        /// <summary>
-        /// Basic Constructor
-        /// </summary>
-        /// <param name="path">file path of the sound</param>
-        public Sound(string path)
-        {
-            Path = path;
+            buffer = AL.GenBuffer();
+            source = AL.GenSource();
+            this.path = path;
+            this.name = name;
+            volume = 100;
+            loopState = false;
+
+            soundData = loadSound(File.Open(path, FileMode.Open), out channels, out bytes, out samples);
         }
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="path">file path of the sound</param>
-        /// <param name="name">name of the sound</param>
-        /// <param name="volume">volume of the sound, values higher than 100 get clamped to 100</param>
-        public Sound(string path, string name, byte volume = 50)
+        /// <param name="path">Filepath for the sound file</param>
+        /// <param name="name">Name of the sound</param>
+        /// <param name="volume">Volume of the sound</param>
+        public Sound(string path, string name, int volume)
         {
-            player = new WindowsMediaPlayer();
-            Path = AppDomain.CurrentDomain.BaseDirectory + path;
-            Name = name;
-            setVolume(volume);
+            buffer = AL.GenBuffer();
+            source = AL.GenSource();
+            this.path = path;
+            this.name = name;
+            volume = 100;
+            loopState = false;
+
+            soundData = loadSound(File.Open(path, FileMode.Open), out channels, out bytes, out samples);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path">Filepath for the sound file</param>
+        /// <param name="name">Name of the sound</param>
+        /// <param name="volume">Volume of the sound</param>
+        /// <param name="loopState">Whether the sound should loop or not</param>
+        public Sound(string path, string name, int volume, bool loopState)
+        {
+            buffer = AL.GenBuffer();
+            source = AL.GenSource();
+            this.path = path;
+            this.name = name;
+            this.volume = volume;
+            this.loopState = loopState;
+
+            soundData = loadSound(File.Open(path, FileMode.Open), out channels, out bytes, out samples);
         }
 
         public virtual void Play()
         {
-            try
-            {
-                Player = new WindowsMediaPlayer();
-                Player.settings.volume = volume;
-                Player.URL = Path;
-            } catch
-            {
-                Debug.WriteLine("Thready Busy, Action Skipped");
-            }
+            AL.BufferData(buffer,GetSoundFormat(channels, bytes), buffer, soundData.Length, samples);
         }
 
-        public virtual void PlayLoop()
+        public virtual void Pause()
         {
-            try
-            {
-                Player = new WindowsMediaPlayer();
-                Player.settings.volume = volume;
-                Player.URL = Path;
-                Player.settings.setMode("loop", true);
-            } catch
-            {
-                Debug.WriteLine("Thready Busy, Action Skipped");
-            }
+        }
+
+        public virtual void Stop()
+        {
         }
 
         public virtual void setVolume(byte v)
         {
-            if (Player == null)
-            {
-                return;
-            }
-            if (v < 100)
-            {
-                Volume = v;
-            }
-            else
-            {
+        }
 
-                Volume = 100;
+        public ALSourceState queryState()
+        {
+            AL.GetSource(source, ALGetSourcei.SourceState, out int state);
+            return (ALSourceState) state;
+        }
+
+        public static ALFormat GetSoundFormat(int channels, int bits)
+        {
+            switch (channels)
+            {
+                case 1: return bits == 8 ? ALFormat.Mono8 : ALFormat.Mono16;
+                case 2: return bits == 8 ? ALFormat.Stereo8 : ALFormat.Stereo16;
+                default: throw new NotSupportedException("The specified sound format is not supported.");
             }
-            Player.settings.volume = Volume;
+        }
+
+        private byte[] loadSound(Stream stream, out int Channels, out int bytesPerSample, out int sampleRate)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                int format_chunk_size = reader.ReadInt32();
+                int audio_format = reader.ReadInt16();
+                int num_channels = reader.ReadInt16();
+                int sample_rate = reader.ReadInt32();
+                int byte_rate = reader.ReadInt32();
+                int block_align = reader.ReadInt16();
+                int bits_per_sample = reader.ReadInt16();
+
+                string data_signature = new string(reader.ReadChars(4));
+                if (data_signature != "data")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int data_chunk_size = reader.ReadInt32();
+
+                Channels = num_channels;
+                bytesPerSample = bits_per_sample;
+                sampleRate = sample_rate;
+
+                return reader.ReadBytes((int)reader.BaseStream.Length);
+            }
         }
     }
 }
--- FullMetalAkari/Game/Objects/Sound/gameMusic.cs
+++ FullMetalAkari/Game/Objects/Sound/gameMusic.cs
@@ -8,69 +8,14 @@ using WMPLib;
 #nullable enable
 namespace FullMetalAkari.Game.Objects.Sounds
 {
-    enum SoundStatus{
-
-    }
-
-    //TODO: fix this to make it actually Async, not running off the main loop.
     public class gameMusic : Sound
     {
-        public enum currentlyPlaying {
-            Intro,
-            Body,
-            Outro
+        public gameMusic(string path, string name) : base(path, name)
+        {
         }
 
-        public enum loopState
+        public gameMusic(string path, string name, int volume) : base(path, name, volume)
         {
-            Loop,
-            NoLoop
-        }
-
-        private string? intro;
-        private currentlyPlaying current;
-        private loopState loop;
-        protected string? Intro { get => intro; set => intro = value; }
-        public currentlyPlaying Current { get => current; set => current = value; }
-        public loopState Loop { get => loop; set => loop = value; }
-
-        public gameMusic(string songPath, string? introPath, string name, loopState loop, byte volume = 50)
-        {
-            Path = AppDomain.CurrentDomain.BaseDirectory+ songPath;
-            Intro = AppDomain.CurrentDomain.BaseDirectory+ introPath;
-            Name = name;
-            Volume = volume;
-            Loop = loop;
-        }
-
-        public void onUpdate()
-        {
-            try
-            {
-                if (Player.playState == WMPPlayState.wmppsStopped && current == currentlyPlaying.Intro)
-                {
-                    Player = new WindowsMediaPlayer();
-                    Player.settings.volume = Volume;
-                    Player.URL = Path;
-                    current = currentlyPlaying.Body;
-                    Player.controls.play();
-                    if (Loop == loopState.Loop)
-                    {
-                        Player.settings.setMode("loop", true);
-                    }
-                }
-            } catch
-            {
-                Debug.WriteLine("Thready Busy, Action Skipped");
-            }
-        }
-
-        public void startSong()
-        {
-            Player = new WindowsMediaPlayer();
-            Player.settings.volume = Volume;
-            Player.URL = Intro;
-            current = currentlyPlaying.Intro;
         }
     }
 }
